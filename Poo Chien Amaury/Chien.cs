734ded6db8dd618aab4f55e143e26d2f2a60a239
blindeur <aibachamaury@gmail.com>
2025-09-29 10:00:01 +0200

    ACT 1 POO : PREMIÈRE APPROCHE
    ex sur chien

---
+++ Poo Chien Amaury/Chien.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poo_Chien_Amaury
+{
+    internal class Chien
+    {
+
+        // Attributs
+        private string _nom;
+        private string _race;
+        private int _age;
+        private string _pelage;
+        private string _taille; // petit, moyen, grand
+        private string _poids; // léger, moyen, lourd
+        private bool _agressivite;
+        private bool _sociabilite; // avec les autres chiens et les humains
+        private string _tag; // collier avec puce électronique
+
+
+        // Constructeur
+        public Chien(string nom, string race, int age, string pelage,string taille ,string poids, bool agressivite, bool sociabilite, string tag)
+        {
+            _nom = nom;
+            _race = race;
+            _age = age;
+            _pelage = pelage;
+            _poids = poids;
+            _taille = taille;
+            _agressivite = agressivite;
+            _sociabilite = sociabilite;
+            _tag = tag;
+        }
+        // Méthodes
+        public string RaceChien()
+        {
+            return $"{_nom} est un {_race}";
+        }
+        public string Aboyer()
+        {
+            return "Woof Woof!";
+        }
+        public string Manger(string nourriture)
+        {
+            return $"{_nom} mange {nourriture}.";
+        }
+        public string Jouer(string jouet)
+        {
+            return $"{_nom} joue avec {jouet}.";
+        }
+        public string SeFaireCaresser()
+        {
+            if (_sociabilite)
+            {
+                return $"{_nom} aime se faire caresser!";
+            }
+            else
+            {
+                return $"{_nom} n'aime pas se faire caresser.";
+            }
+        }
+        public string FaireUnePromenade(string lieu)
+        {
+            return $"{_nom} fait une promenade à {lieu}.";
+        }
+
+        public string AfficherInfos()
+        {
+            return $"Nom: {_nom} " + $" age: {_age} ans " + $" taille: {_taille} cm " + $" pelage: son pelage est {_pelage} " + $" tag: son tag est {_tag} ";
+
+        }
+       
+        
+    }
+}
---
+++ Poo Chien Amaury/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace Poo_Chien_Amaury
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Chien[] ToutLesChiens = new Chien[4];
+
+
+            ToutLesChiens[0] = new Chien("Kage", "Berger Allemand", 5, "Long", "65", "grand", false, true, "12345ABC");
+            ToutLesChiens[1] = new Chien("Nova", "Husky de Sibérie", 3, "Long", "63", "grand", true, false, "78945CFT");
+            ToutLesChiens[2] = new Chien("Ghost", "Malamute de l'Alaska", 4, "Long", "60", "grand", false, true, "16483SPQ");
+            ToutLesChiens[3] = new Chien("Balto", "Dobermann", 2, "Court", "80", "grand", false, true, "65347LCD");
+            for (int i = 0; i < ToutLesChiens.Length; i++)
+            {
+                Console.WriteLine(ToutLesChiens[i].RaceChien());
+                Console.WriteLine(ToutLesChiens[i].Aboyer());
+                Console.WriteLine(ToutLesChiens[i].Manger("croquettes"));
+                Console.WriteLine(ToutLesChiens[i].Jouer("balle"));
+                Console.WriteLine(ToutLesChiens[i].SeFaireCaresser());
+                Console.WriteLine(ToutLesChiens[i].FaireUnePromenade("parc"));
+                Console.WriteLine(ToutLesChiens[i].AfficherInfos());
+            }
+           
+
+        }
+    }
+}
